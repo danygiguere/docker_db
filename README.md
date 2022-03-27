85ee333eefe641d33a5ce84a757c41736826e74f
Dany Giguere <danygiguere23@gmail.com>
2022-03-27 19:32:00 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# README #
+
+### Local Installation
+- First, make sure you have Docker installed on your computer.
+- second, run `cp .env.example .env` and add your credentials
+- Then make sure Docker is up and run this command: `sh install.sh`
+
+to stop the db docker image, run  `docker-compose down`
+to restart it without rebuilding, run  `docker-compose up`
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#This script is for local installation only
+
+#export .env vars for this script
+if [ -f .env ]; then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+docker-compose down || exit
+docker-compose up -d
+
+sleep 10 #Give mysql 10sec to start up before droping database
+docker-compose exec db mysql -uroot -p${DB_PASSWORD} -e "DROP DATABASE IF EXISTS ${DB_DATABASE};"
+sleep 10 #Give mysql 10sec to drop database before creating a new one
+docker-compose exec db mysql -uroot -p${DB_PASSWORD} -e "CREATE DATABASE ${DB_DATABASE};"
+
+docker-compose down || exit
+docker-compose up
